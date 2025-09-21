22b64018a1f2bae2645425ce411cbcdf2456622c
FATAL ARCADE <fatal_arcade@yahoo.com>
2025-09-21 15:47:45 -0700

    Delete main.js

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-
-const toggleBtn = document.getElementById("toggle-btn");
-const sidebar = document.getElementById("sidebar");
-
-toggleBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("collapsed");
-});
